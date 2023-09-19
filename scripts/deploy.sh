9390cd3ce1b36fad9215612fd91c2749ed3a4ece
Parker Freestone <parkerfreestone@gmail.com>
2023-09-19 14:06:54 -0600

    :rocket: All the homies hate envoy

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /var/www/highland-hq || exit
+
+git pull origin main
+
+composer install --optimize-autoloader --no-dev
+
+php artisan migrate --force
+
+npm i
+npm run build
+
+php artisan config:cache
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache
+
+echo "Deployment completed successfully!"
